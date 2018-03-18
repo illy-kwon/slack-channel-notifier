a8cebceb5f4b4da20e209f0997e98efa19fb9fe3
IL Kwon <illy-kwon@users.noreply.github.com>
2018-03-18 12:48:21 +0900

    Change channel to post message

--- notifier.rb
+++ notifier.rb
@@ -23,14 +23,15 @@ end
 
 client.on :channel_created do |data|
   text = "A new channel <\##{data.channel.id}> was created by <@#{data.channel.creator}>."
-  channel_id = 'C9SKF3M6K'  # #bot-test channel
+  channel_id = 'C9RKP4ED9'  # #slack-notification channel
   client.message(channel: channel_id, text: text, as_user: true)
 end
 
 client.on :emoji_changed do |data|
+  puts data.subtype
   if data.subtype == "add"
     text = "A new emoji :#{data.name}: was added."
-    channel_id = 'C9SKF3M6K'  # #bot-test channel
+    channel_id = 'C9RKP4ED9'  # #slack-notification channel
     client.message(channel: channel_id, text: text, as_user: true)
   end
 end
